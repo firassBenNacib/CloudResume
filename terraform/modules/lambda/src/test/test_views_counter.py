1c43327a2869b5709854e36c9171cb3eef1883a6
root <root@DESKTOP-UE84V2D>
2025-04-06 08:49:52 +0100

    update lambda test

--- terraform/modules/lambda/src/test/test_views_counter.py
+++ terraform/modules/lambda/src/test/test_views_counter.py
@@ -1,28 +1,22 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import sys
 import os
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../main')))
 
-from views_counter import lambda_handler
+with patch('boto3.resource') as mock_dynamodb_resource:
+    mock_table = MagicMock()
+    mock_table.update_item.return_value = {'Attributes': {'visits': 10}}
+    mock_dynamodb_resource.return_value.Table.return_value = mock_table
+
+    from views_counter import lambda_handler
 
 class TestLambdaHandler(unittest.TestCase):
 
     def test_lambda_handler_success(self):
         event = {}
         context = {}
-
-        fake_response = {
-            'Attributes': {
-                'visits': 10
-            }
-        }
-
-        import views_counter
-        views_counter.table = MagicMock()
-        views_counter.table.update_item.return_value = fake_response
 
         result = lambda_handler(event, context)
 
